7a93751409a84c7198cb99580833bb66534b7c4f
Steven Owens <owenss@oregonstate.edu>
2020-08-05 10:40:32 -0700

    implemented getInput feature, terminates loop with DONE

--- line_processor_st.c
+++ line_processor_st.c
@@ -1,10 +1,29 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+#define BUFFERSIZE 1000
 
 // Gets input from stdin stream
 int getInput(){
+
+    char* input = calloc(1,1), buffer[BUFFERSIZE];
+    printf("Enter a message: \n");
+
+    while(fgets(buffer, BUFFERSIZE, stdin)){
+
+        input = realloc(input, strlen(input) + 1 + strlen(buffer));
+        strcat(input, buffer);
+        input[strlen(input) - 1] = '\0';  // replace '\n' with '\0'
+        if(strcmp(input, "DONE") == 0) break;
+        }
+        memset(buffer, '\0', sizeof(buffer));
+
+    free(input);
     return 0;
 }
+
+
 
 // Parses input to replace line seperators '\n' with ' ' spaces (blanks)
 int parseLines(){
@@ -24,6 +43,7 @@ int writeOutput(){
 int main(int argc, char *argv[]){
 
     fprintf(stdout, "Hello World\n");
+    getInput();
 
     return 0;
 }
