5d2d9b0c980731a4d83d4fe6ca0c70233dfe3ab3
Steven Owens <owenss@oregonstate.edu>
2020-08-05 09:01:35 -0700

    added function stubs for core program requirements

--- line_processor_st.c
+++ line_processor_st.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
 
+// Gets input from stdin stream
+int getInput(){
+    return 0;
+}
+
+// Parses input to replace line seperators '\n' with ' ' spaces (blanks)
+int parseLines(){
+    return 0;
+}
+
+// Parses lines to replace instance of '++' with '^' character
+int parseChars(){
+    return 0;
+}
+// Writes parsed lines to stdout steam
+int writeOutput(){
+    return 0;
+}
+
+
 int main(int argc, char *argv[]){
 
     fprintf(stdout, "Hello World\n");
