1f6b66ca0df3543b25dff93c30a319bb37f898cd
Steven Owens <owenss@oregonstate.edu>
2020-08-06 11:12:55 -0700

    modified compile.sh for single and mult0thread apps

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -std=c99 -Wall -pedantic line_processor_st.c -o line_processor
-./line_processor
+gcc -std=c99 -Wall -pedantic line_processor_st.c -o line_processor_st
+gcc -std=c99 -Wall -pedantic line_processor.c -o line_processor
+#./line_processor
