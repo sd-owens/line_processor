7596917f3a10f9b476930e0df00ff803a7d385f8
Steven Owens <owenss@oregonstate.edu>
2020-08-06 11:46:29 -0700

    increased max size of buffer to 10 lines of 1000 chars and added line producer and consumer threads

--- line_processor.c
+++ line_processor.c
@@ -3,15 +3,32 @@
 #include <string.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <unistd.h>
 
-#define SIZE 1000
+// Sized for 10 input lines of max 1000 chars
+#define SIZE 10000
+
+// MULTI-THREADED VERSION OF LINE PROCESSOR PROGRAM
 
 // Buffers, Shared Thread Resources
 char t1_buffer[SIZE];
 char t2_buffer[SIZE];
 char t3_buffer[SIZE];
 
-// MULTI-THREADED VERSION OF LINE PROCESSOR PROGRAM
+// Number of items in t1 buffer, shared resource
+int t1_count = 0;
+// Number of items in t2 buffer, shared resource
+int t2_count = 0;
+// Number of items in t3 buffer, shared resource
+int t3_count = 0;
+
+
+// Initialize the mutex
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Initialize the condition variables
+pthread_cond_t full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t empty = PTHREAD_COND_INITIALIZER;
 
 // Gets input from stdin stream
 int getInputLine(char *buffer){
@@ -19,6 +36,20 @@ int getInputLine(char *buffer){
     fgets(buffer, SIZE, stdin);
     
     return 0;
+}
+
+void *input_producer(void *args){
+
+    pthread_mutex_lock(&mutex);
+    while(t1_count != 0){
+
+        // t1_buffer is still full
+        pthread_cond_signal(&full);
+    }
+    // Signal to consumer that buffer is not longer empty
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mutex);
+
 }
 
 // Parses input to replace line seperators '\n' with ' ' spaces (blanks)
@@ -38,6 +69,21 @@ int parseLines(char *output, char *input){
         i++;
     }
     return 0;
+}
+
+void *input_consumer(void *args){
+
+    pthread_mutex_lock(&mutex);
+    while(t1_count != 0){
+
+        // t1_buffer is still full
+        pthread_cond_signal(&full);
+    }
+    parseLines(t2_buffer, t1_buffer);
+    // Signal to consumer that buffer is not longer empty
+    pthread_cond_signal(&full);
+    pthread_mutex_unlock(&mutex);
+
 }
 
 // Parses lines to replace instance of '++' with '^' character
@@ -85,16 +131,24 @@ int main(int argc, char *argv[]){
 
     bool cont = true;
 
-    while(cont){
+    pthread_t input, line_seperator;
+    //plus_sign, output;
 
-        getInputLine(t1_buffer);
-        if(strcmp(t1_buffer, "DONE\n") == 0){
-            cont = false;
-            continue;
-        }
-        parseLines(t2_buffer, t1_buffer);
-        parseChars(t3_buffer, t2_buffer);
-        writeOutput(t3_buffer);
-    }
+    pthread_create(&input, NULL, input_producer, NULL);
+    pthread_create(&line_seperator, NULL, input_consumer, NULL);
+    pthread_join(input, NULL);
+    pthread_join(line_seperator, NULL);
+
+    // while(cont){
+
+    //     getInputLine(t1_buffer);
+    //     if(strcmp(t1_buffer, "DONE\n") == 0){
+    //         cont = false;
+    //         continue;
+    //     }
+    //     parseLines(t2_buffer, t1_buffer);
+    //     parseChars(t3_buffer, t2_buffer);
+    //     writeOutput(t3_buffer);
+    // }
     return 0;
 }
