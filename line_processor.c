3530e8c0fd615e63de4c22a69493509ac7c23935
Steven Owens <owenss@oregonstate.edu>
2020-08-06 11:11:42 -0700

    renamed global variables

---
+++ line_processor.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#define SIZE 1000
+
+// MULTI-THREADED VERSION OF LINE PROCESSOR
+
+// Gets input from stdin stream
+int getInputLine(char *buffer){
+
+    fgets(buffer, BUFFERSIZE, stdin);
+    
+    return 0;
+}
+
+// Parses input to replace line seperators '\n' with ' ' spaces (blanks)
+int parseLines(char *output, char *input){
+    
+    strcat(output, input);
+
+    // Clear the input buffer for reuse
+    memset(input, '\0', strlen(input));
+
+    int i = 0;
+    while (output[i] != '\0'){
+
+        if(output[i] == '\n'){
+            output[i] = ' '; 
+        }
+        i++;
+    }
+    return 0;
+}
+
+// Parses lines to replace instance of '++' with '^' character
+int parseChars(char *output, char *input){
+
+    strcat(output, input);
+
+    // Clear the input buffer for reuse
+    memset(input, '\0', strlen(input));
+
+    int i = 0;
+    while (output[i] != '\0'){
+
+        if(output[i] == '+' && output[i+1] == '+'){
+
+            output[i] = '^';
+            // Copy all chars left one position to fill gap created.
+            strcpy(output + i + 1, output + i + 2);
+        }
+        i++;
+    }
+    return 0;
+}
+// Writes parsed lines to stdout steam
+int writeOutput(char *buffer){
+
+    // Print 80 char limit line to terminal
+    while (strlen(buffer) > 79) {
+
+        int i = 0;
+        while (i < 79){
+
+            fputc(buffer[i], stdout);
+            i++;
+        }
+        fputc('\n', stdout);
+
+        // Update buffer by overwriting written characters
+        strcpy(buffer, buffer + 79);
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+
+    char t1_buffer[SIZE];
+    char t2_buffer[SIZE];
+    char t3_buffer[SIZE];
+
+    bool cont = true;
+
+    while(cont){
+
+        getInputLine(t1_buffer);
+        if(strcmp(t1_buffer, "DONE\n") == 0){
+            cont = false;
+            continue;
+        }
+        parseLines(t2_buffer, t1_buffer);
+        parseChars(t3_buffer, t2_buffer);
+        writeOutput(t3_buffer);
+    }
+    return 0;
+}
